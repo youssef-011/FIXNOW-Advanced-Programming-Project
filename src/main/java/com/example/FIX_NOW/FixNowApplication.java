5dacd318cf69d6675c87b29fcdd14a9e127e366a
Yossef Zanhome <yossefzanhome@gmail.com>
2026-04-24 18:10:49 +0300

    add spring boot project

---
+++ src/main/java/com/example/FIX_NOW/FixNowApplication.java
@@ -0,0 +1,13 @@
+package com.example.FIX_NOW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixNowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FIX_NOW/FixNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FIX_NOW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
